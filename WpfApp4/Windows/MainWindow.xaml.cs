3538ee39573574b9711a4b31543ac5970d011197
Talib_ip88 <Talib_ip88@STHQ0125-13>
2024-04-24 18:01:48 +0400

    Add project files.

---
+++ WpfApp4/Windows/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp4.Models;
+
+namespace WpfApp4.Windows;
+
+public partial class MainWindow : Window
+{
+    public ObservableCollection<Car>? Cars { get; set; }
+    public MainWindow()
+    {
+        Cars = new ObservableCollection<Car>();
+        Cars.Add(new Car() { Model = "Nissan", Vendor = "KIa", Year = "1222" });
+        Cars.Add(new Car() { Model = "Hunday", Vendor = "KIa", Year = "1222" });
+        Cars.Add(new Car() { Model = "BMW", Vendor = "KIa", Year = "1222" });
+        InitializeComponent();
+        DataContext = this;
+    }
+
+    private void ListView_PreviewMouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        Update_AddWindow update_AddWindow = new Update_AddWindow();
+        update_AddWindow.Car = (sender as ListView).SelectedItem as Car;
+        update_AddWindow.Text = "Update";
+        update_AddWindow.ShowDialog();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Update_AddWindow update_AddWindow = new Update_AddWindow();
+        update_AddWindow.Text = "Add";
+        update_AddWindow.ShowDialog();
+        Cars.Add(update_AddWindow.Car);
+    }
+}
---
+++ WpfApp4/Windows/Update_AddWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using WpfApp4.Models;
+
+namespace WpfApp4.Windows;
+
+public partial class Update_AddWindow : Window, INotifyPropertyChanged
+{
+    private Car? car;
+    private string? text;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public string? Text { get => text; set { text = value;  OnPropertyChanged(); } }
+    public Car? Car { get => car; set { car = value; OnPropertyChanged(); } }
+    public Update_AddWindow()
+    {
+        InitializeComponent();
+        DataContext = this;
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+
+    private void AddUpdateButton_Click(object sender, RoutedEventArgs e)
+    {
+        if(AddUpdateButton.Content.ToString() == "Update")
+        {
+            DialogResult = false;
+            return;
+        }   
+        else
+        {
+            car = new();
+            car.Model = ModelBox.Text;
+            car.Year = YearBox.Text;
+            car.Vendor = VendorBox.Text;
+            DialogResult = false;
+            return;
+        }
+
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        if (AddUpdateButton.Content.ToString() == "Update")
+        {
+            DialogResult = false;
+            return;
+        }
+        else
+        {
+            car = new();
+            car.Model = ModelBox.Text;
+            car.Year = YearBox.Text;
+            car.Vendor = VendorBox.Text;
+            DialogResult = false;
+            return;
+        }
+    }
+}
